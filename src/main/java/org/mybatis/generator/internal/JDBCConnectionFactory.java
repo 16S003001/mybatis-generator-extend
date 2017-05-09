23e516d44d03e21683ab9056c485f72e7bdc6cb7
Guo Yonghui <16S003001@stu.hit.edu.cn>
2017-05-09 13:53:24 +0800

    在生成的实体类中增加对表的注释的生成

--- src/main/java/org/mybatis/generator/internal/JDBCConnectionFactory.java
+++ src/main/java/org/mybatis/generator/internal/JDBCConnectionFactory.java
@@ -1,17 +1,17 @@
 /**
- *    Copyright 2006-2016 the original author or authors.
- *
- *    Licensed under the Apache License, Version 2.0 (the "License");
- *    you may not use this file except in compliance with the License.
- *    You may obtain a copy of the License at
- *
- *       http://www.apache.org/licenses/LICENSE-2.0
- *
- *    Unless required by applicable law or agreed to in writing, software
- *    distributed under the License is distributed on an "AS IS" BASIS,
- *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *    See the License for the specific language governing permissions and
- *    limitations under the License.
+ * Copyright 2006-2016 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package org.mybatis.generator.internal;
 
@@ -31,7 +31,7 @@ import org.mybatis.generator.config.JDBCConnectionConfiguration;
  * but also to make sure that any native libraries are only loaded one time
  * (avoids the dreaded UnsatisfiedLinkError library loaded in another
  * classloader)
- * 
+ *
  * @author Jeff Butler
  */
 public class JDBCConnectionFactory implements ConnectionFactory {
@@ -45,7 +45,7 @@ public class JDBCConnectionFactory implements ConnectionFactory {
     /**
      * This constructor is called when there is a JDBCConnectionConfiguration
      * specified in the configuration.
-     * 
+     *
      * @param config
      */
     public JDBCConnectionFactory(JDBCConnectionConfiguration config) {
@@ -56,10 +56,10 @@ public class JDBCConnectionFactory implements ConnectionFactory {
         driverClass = config.getDriverClass();
         otherProperties = config.getProperties();
     }
-    
+
     /**
-     * This constructor is called when this connection factory is specified 
-     * as the type in a ConnectionFactory configuration element. 
+     * This constructor is called when this connection factory is specified
+     * as the type in a ConnectionFactory configuration element.
      */
     public JDBCConnectionFactory() {
         super();
@@ -78,6 +78,9 @@ public class JDBCConnectionFactory implements ConnectionFactory {
         if (stringHasValue(password)) {
             props.setProperty("password", password); //$NON-NLS-1$
         }
+
+        props.setProperty("remarksReporting", "true");
+        props.setProperty("useInformationSchema", "true");
 
         props.putAll(otherProperties);
 
@@ -111,10 +114,10 @@ public class JDBCConnectionFactory implements ConnectionFactory {
         password = properties.getProperty("password");
         connectionURL = properties.getProperty("connectionURL");
         driverClass = properties.getProperty("driverClass");
-        
+
         otherProperties = new Properties();
         otherProperties.putAll(properties);
-        
+
         // remove all the properties that we have specific attributes for
         otherProperties.remove("userId");
         otherProperties.remove("password");
